15b0798ab6f3a05bffb4c126155924e30884f1b9
davidRamalho <david.m.ramalho@gmail.com>
2021-01-18 20:58:48 -0700

    d2

--- express_server.js
+++ express_server.js
@@ -43,6 +43,16 @@ app.get("/urls/:shortURL", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  console.log(req.body); // Log the POST request body to the console
+  const randomShortURL = generateRandomString();
+  urlDatabase[randomShortURL] = (req.body.longURL);
+  res.redirect(`/urls/${randomShortURL}`);         // Respond with 'Ok' (we will replace this)
+  console.log(urlDatabase);
 });
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
+
